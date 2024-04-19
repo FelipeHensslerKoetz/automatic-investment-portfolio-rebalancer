56526dfee597d25fa733e5dffafd611d1b279c25
Felipe Hnessler Koetz <felipehenssler@gmail.com>
2024-04-19 16:06:15 -0300

    fix: api versions

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
 class ApiConstraints
+  attr_reader :version, :default
+
   def initialize(options)
     @version = options[:version]
     @default = options[:default]
   end
 
   def matches?(req)
-    @default || req.headers['Accept'].include?("application/vnd.blog.v#{@version}")
+    return true if default && !req.headers['Accept'].include?("application/vnd.blog.v")
+
+    req.headers['Accept'].include?("application/vnd.blog.v#{version}")
   end
 end
