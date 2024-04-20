2ea9f6215100dc9f5dd40b35b71ae01e2f493562
Felipe Hnessler Koetz <felipehenssler@gmail.com>
2024-04-19 22:54:38 -0300

    feat: create Partner model

---
+++ db/migrate/20240420015153_create_partners.rb
@@ -0,0 +1,13 @@
+class CreatePartners < ActiveRecord::Migration[6.1]
+  def change
+    create_table :partners do |t|
+      t.string :name, null: false
+      t.string :description
+      t.string :url
+
+      t.timestamps
+    end
+
+    add_index :partners, :name, unique: true
+  end
+end
